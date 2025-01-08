71e36d930ad8d5c83ea54c50d60626748ac662d3
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-01-08 09:29:38 -0600

    test: setup test config, tested homepage to ensure items render

---
+++ client/src/_tests_/Home.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import Home from "../components/Home";
+
+describe("home page renders correct elements", () => {
+	it("renders all items on the page", () => {
+		render(<Home />);
+
+		//X logo renders
+		expect(screen.getByAltText("X logo"));
+
+		//headers render
+		expect(screen.getByText("Happening now"));
+		expect(screen.getByText("Join today."));
+
+		//links render
+		expect(screen.getByText("Continue with Google"));
+		expect(screen.getByText("Create Account"));
+		expect(screen.getByText("Log In"));
+		expect(screen.getByText("Sign In as Guest"));
+	});
+});
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { configDefaults } from 'vitest/config'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 /// <reference types="vitest" />
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    exclude: [...configDefaults.exclude],
-  }
-})
+	plugins: [react()],
+	test: {
+		exclude: [...configDefaults.exclude],
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./tests/setup.js",
+	},
+});
