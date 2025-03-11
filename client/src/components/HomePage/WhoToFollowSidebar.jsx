0080978c5e4b213c689915503a759451d43f7a48
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2025-03-11 14:41:46 -0500

    remove loggedInUserInfo from whotofollow sidebar

--- client/src/components/HomePage/WhoToFollowSidebar.jsx
+++ client/src/components/HomePage/WhoToFollowSidebar.jsx
@@ -4,12 +4,15 @@ import useFetchUsers from "../../helpers/useFetchUsers";
 import "../../Styles/WhoToFollowSidebar.css";
 import CircularProgress from "@mui/material/CircularProgress";
 import DisplayedUsers from "./DisplayedUsers";
+import useGetLoggedInUserProfileInfo from "../../helpers/useGetLoggedInUserProfileInfo";
 
 export default function WhoToFollowSidebar() {
 	const { refreshTrigger } = useContext(RefreshContext);
 
 	const { users, isLoading, error } = useFetchUsers("user", refreshTrigger);
+	const { profileInfo } = useGetLoggedInUserProfileInfo();
 
+	const filteredUsers = users?.filter((user) => user?.name !== profileInfo?.user.name)
 	if (isLoading) {
 		return <CircularProgress />;
 	}
@@ -21,7 +24,7 @@ export default function WhoToFollowSidebar() {
 	return (
 		<div className="who-to-follow-sidebar" style={{ width: "100%" }}>
 			<h1 style={{ color: "white" }}>Who to Follow</h1>
-			<DisplayedUsers users={users} />
+			<DisplayedUsers users={filteredUsers} />
 		</div>
 	);
 }
